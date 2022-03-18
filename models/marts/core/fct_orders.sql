90f01a0999a1eec00cef81c112734a28faf3fb14
Elize Papineau <elize.papineau@fishtownanalytics.com>
2022-03-18 00:28:48 +0000

    Fix fct_order fanout causing dbt test error

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -9,44 +9,29 @@ with order_item as (
     select * from {{ ref('order_items_joined') }}
 
 ),
-order_item_summary as (
 
-    select 
-        order_id,
-        sum(gross_item_sales_amount) as gross_item_sales_amount,
-        sum(item_discount_amount) as item_discount_amount,
-        sum(item_tax_amount) as item_tax_amount,
-        sum(net_item_sales_amount) as net_item_sales_amount
-    from order_item
-    group by
-        1
-),
 final as (
 
     select 
 
-        order_item.order_id, 
-        order_item.order_date,
-        order_item.customer_id,
-        order_item.order_status_code,
-        order_item.priority_code,
-        order_item.clerk_name,
-        order_item.ship_priority,
+        order_id, 
+        order_date,
+        customer_id,
+        order_status_code,
+        priority_code,
+        clerk_name,
+        ship_priority,
                 
         1 as order_count,                
-        order_item_summary.gross_item_sales_amount,
-        order_item_summary.item_discount_amount,
-        order_item_summary.item_tax_amount,
-        order_item_summary.net_item_sales_amount
-    from
-        order_item
-    inner join order_item_summary
-            on order_item.order_id = order_item_summary.order_id
-)
-select 
-    *
-from
-    final
+        sum(gross_item_sales_amount) as gross_item_sales_amount,
+        sum(item_discount_amount) as item_discount_amount,
+        sum(item_tax_amount) as item_tax_amount,
+        sum(net_item_sales_amount) as net_item_sales_amount
 
-order by
-    order_date
+    from order_item
+    {{ dbt_utils.group_by(n = 8) }}
+
+)
+
+select * from final
+order by order_date
