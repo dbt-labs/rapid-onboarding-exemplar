97548e0067a576e694518005fd2e926bf6f0ea77
Carol Ohms <carol.ohms@fishtownanalytics.com>
2022-07-12 21:00:02 +0000

    added test field

--- models/staging/tpch/stg_tpch_customers.sql
+++ models/staging/tpch/stg_tpch_customers.sql
@@ -17,7 +17,8 @@ renamed as (
         c_phone as phone_number,
         c_acctbal as account_balance,
         c_mktsegment as market_segment,
-        c_comment as comment
+        c_comment as comment,
+        false as is_false
 
     from source
 
