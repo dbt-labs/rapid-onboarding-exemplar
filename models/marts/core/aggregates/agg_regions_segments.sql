b461f0741e580f7977183234ed51dade4183d250
Nicholas A. Yager <nicholas.yager@fishtownanalytics.com>
2022-08-15 16:33:05 +0000

    Trivial Change

--- models/marts/core/aggregates/agg_regions_segments.sql
+++ models/marts/core/aggregates/agg_regions_segments.sql
@@ -22,7 +22,8 @@ final as (
         customers.market_segment,
         round(sum(agg_orders.total_sales),2) as total_sales
     from customers
-    left join agg_orders on customers.customer_id = agg_orders.customer_id
+    left join agg_orders on 
+        customers.customer_id = agg_orders.customer_id
     group by 1, 2
 )
 
