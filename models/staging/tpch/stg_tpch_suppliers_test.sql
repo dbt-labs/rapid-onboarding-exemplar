872fa281bcf8c31e4387f7df5c01e7a5ffd539fd
vikalgupta <vikal.gupta@hashicorp.com>
2022-09-08 19:51:33 +0000

    changed schema- added stg_tpch_suppliers_test

---
+++ models/staging/tpch/stg_tpch_suppliers_test.sql
@@ -0,0 +1,27 @@
+with source as (
+
+    select * from {{ source('tpch', 'supplier') }}
+
+),
+
+renamed as (
+
+    select
+
+        -- ids
+        s_suppkey as supplier_id,
+        s_nationkey as nation_id,
+
+        -- descriptions
+        s_name as supplier_name,
+        s_address as supplier_address,
+        s_phone as phone_number,
+
+        -- amounts
+        s_acctbal as account_balance
+
+    from source
+
+)
+
+select * from renamed
