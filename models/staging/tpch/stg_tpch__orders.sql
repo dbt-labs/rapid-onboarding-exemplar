c776fcd8f6e8d6ab3144ea8e38499e93463bb183
Pat Kearns <pat.kearns@dbtlabs.com>
2022-11-02 03:47:10 +0000

    fix accidental change

--- models/staging/tpch/stg_tpch__orders.sql
+++ models/staging/tpch/stg_tpch__orders.sql
@@ -7,15 +7,24 @@ with source as (
 renamed as (
 
     select
+        -- ids
         o_orderkey as order_id,
         o_custkey as customer_id,
-        o_orderstatus as order_status_code,
-        o_totalprice as total_price,
-        o_orderdate as order_date,
-        o_orderpriority as priority_code,
+
+        -- descriptions
+        o_comment as comment,
         o_clerk as clerk_name,
+
+        -- numbers
+        o_totalprice as total_price,
+
+        -- statuses
+        o_orderstatus as order_status_code,
+        o_orderpriority as priority_code,
         o_shippriority as ship_priority,
-        o_comment as order_comment
+
+        -- dates
+        o_orderdate as order_date
 
     from source
 
