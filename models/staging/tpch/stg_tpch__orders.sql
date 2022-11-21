cf9b95c7a72758dbc4494aaca10f719ba8a7e8d9
Nicholas Yager <nicholas.yager@fishtownanalytics.com>
2022-11-21 14:38:58 +0000

    Reformatted stg__tpch_orders

--- models/staging/tpch/stg_tpch__orders.sql
+++ models/staging/tpch/stg_tpch__orders.sql
@@ -1,34 +1,32 @@
-with source as (
+with
+    source as (select * from {{ source("tpch", "orders") }}),
 
-    select * from {{ source('tpch', 'orders') }}
+    renamed as (
 
-),
+        select
 
-renamed as (
+            -- ids
+            o_orderkey as order_id,
+            o_custkey as customer_id,
 
-    select
+            -- descriptions
+            o_comment as comment,
+            o_clerk as clerk_name,
 
-        -- ids
-        o_orderkey as order_id,
-        o_custkey as customer_id,
-        
-        -- descriptions
-        o_comment as comment,
-        o_clerk as clerk_name,
+            -- numbers
+            o_totalprice as total_price,
 
-        -- numbers
-        o_totalprice as total_price,
+            -- statuses
+            o_orderstatus as order_status_code,
+            o_orderpriority as priority_code,
+            o_shippriority as ship_priority,
 
-        -- statuses
-        o_orderstatus as order_status_code,
-        o_orderpriority as priority_code,
-        o_shippriority as ship_priority,
+            -- dates
+            o_orderdate as order_date
 
-        -- dates
-        o_orderdate as order_date
+        from source
 
-    from source
+    )
 
-)
-
-select * from renamed
+select *
+from renamed
