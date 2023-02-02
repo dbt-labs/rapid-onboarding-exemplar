376fd4a660bca40e1d38122afa406bd25f91c77e
Dan Horner <dan.horner@fishtownanalytics.com>
2023-02-02 16:07:39 +0000

    removed intermediate from models path in project.yml, added generic test asserting positive none-zero field valuse

---
+++ tests/generic/assert_positive_none_zero_field.sql
@@ -0,0 +1,21 @@
+{% test is_positive_and_not_zero(model, column_name) %}
+
+    {{
+        config(
+            enabled=true,
+            error_if = '>50',
+            warn_if = '>5',
+            tags = ['my_generic_test']
+        )
+    }}
+
+    with validation as (
+        select {{ column_name }} as positive_value
+        from {{ model }}
+    )
+
+    select *
+    from validation
+    where positive_value <= 0
+
+{% endtest %}
