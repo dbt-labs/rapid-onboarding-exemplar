024768359102d63331da0448b98b726f5bcf4534
Jeremy Holtzman <jeremy.holtzman@fishtownanalytics.com>
2023-05-17 16:58:16 +0000

    added tests from dbt utils and expectations

---
+++ models/examples/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
