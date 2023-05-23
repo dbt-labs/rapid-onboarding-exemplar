f93fb319417147d824d5dfc222a8ffa21e915c9d
Dan Horner <dan.horner@fishtownanalytics.com>
2023-05-23 15:17:05 +0000

    demonstrate ci job

--- models/marts/finance/fct_order_items.sql
+++ models/marts/finance/fct_order_items.sql
@@ -1,9 +1,7 @@
 {{
     config(
         tags = ['finance'],
-        materialized='incremental',
-        unique_key='order_item_id',
-         incremental_strategy='delete+insert'
+
     )
 }}
 
@@ -59,10 +57,6 @@ select
     *
 from
     final
-
-{% if is_incremental() %}
-where commit_date > (select max(commit_date) from {{ this }} )
-{% endif %}
 
 order by
     order_date
