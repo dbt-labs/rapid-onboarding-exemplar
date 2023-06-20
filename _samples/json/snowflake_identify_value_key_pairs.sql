932e81a6d294519060a65b0698c7b7a046488914
Logan Cochran <logan.cochran@dbtlabs.com>
2023-06-20 19:26:27 +0000

    update json example names and add flatten docs

--- _samples/json/snowflake_identify_value_key_pairs.sql
+++ _samples/json/snowflake_identify_value_key_pairs.sql
@@ -1,4 +1,5 @@
 -- this will provide you with every key value pair using Snowflake functionality, which will then be leverage in the stg_model
+-- flatten docs: https://docs.snowflake.com/en/sql-reference/functions/flatten
 
 select distinct path
 from raw.tpch_json.customer_json, 
