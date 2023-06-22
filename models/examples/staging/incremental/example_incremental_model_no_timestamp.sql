673f92b449d4854ca1b30000b0e2b0235be826fb
Jeremy Holtzman <jeremy.holtzman@fishtownanalytics.com>
2023-06-22 16:32:32 +0000

    more incremental examples

--- models/examples/staging/incremental/example_incremental_model_no_timestamp.sql
+++ models/examples/staging/incremental/example_incremental_model_no_timestamp.sql
@@ -9,7 +9,7 @@ with source as (
     select * from {{ ref('example_source_for_incremental') }}
     {% if is_incremental() %}
         -- this filter will only be applied on an incremental run
-        where _etl_loaded_at > (select max(_etl_loaded_at) from {{ this }}) 
+        where id not in (select id from {{ this }}) 
     {% endif %}
 )
 
--- models/examples/staging/incremental/example_incremental_model_with_unique_key.sql
+++ models/examples/staging/incremental/example_incremental_model_with_unique_key.sql
@@ -1,6 +1,7 @@
 {{
     config(
         materialized='incremental',
+        unique_key='id',
         on_schema_change='sync_all_columns'
     )
 }}
@@ -9,7 +10,9 @@ with source as (
     select * from {{ ref('example_source_for_incremental') }}
     {% if is_incremental() %}
         -- this filter will only be applied on an incremental run
-        where _etl_loaded_at > (select max(_etl_loaded_at) from {{ this }}) 
+        where _etl_loaded_at > (select {{ dbt.dateadd("hour", -3, "max(_etl_loaded_at)") }} from {{ this }}) 
+
+        
     {% endif %}
 )
 
