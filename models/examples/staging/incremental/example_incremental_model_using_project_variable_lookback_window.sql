0135a4b3b4c99deb6a2cbada610d8f1c822bc6cd
Jeremy Holtzman <jeremy.holtzman@fishtownanalytics.com>
2023-06-23 17:02:10 +0000

    incremental model with variable lookback window

---
+++ models/examples/staging/incremental/example_incremental_model_using_project_variable_lookback_window.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id',
+        on_schema_change='sync_all_columns'
+    )
+}}
+
+with source as (
+    select * from {{ ref('example_source_for_incremental') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where _etl_loaded_at > (
+            
+            select {{ dbt.dateadd(
+                var('incremental_lookback_period'), 
+                -var('incremental_lookback_value'), 
+                "max(_etl_loaded_at)") }} 
+            
+            from {{ this }}
+        ) 
+
+        
+    {% endif %}
+
+    
+)
+
+select *
+from source
