2c4290dc89d2a3f1892e5684ca76e090d0f0b789
Jess Williams <jess@dbtlabs.com>
2023-11-27 17:13:23 +0000

    make barebones project

--- models/_samples/staging/my_unorganized_model.sql
+++
@@ -1,8 +0,0 @@
-
-select
-    customers.id as customer_id,
-    orders.id as order_id,
-    orders.status as order_status
-from {{ source('jaffle_shop', 'customers') }} customers 
-join {{ source('jaffle_shop', 'orders') }} orders 
-on customers.id = orders.user_id
