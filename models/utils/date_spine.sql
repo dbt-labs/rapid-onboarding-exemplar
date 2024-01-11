97518196aec6bd4518d69c86aed67a2cdc054c61
Kyle Coapman <kcoapman@fishtownanalytics.com>
2024-01-11 18:17:51 +0000

    small tweaks to get things working

--- models/utils/date_spine.sql
+++ models/utils/date_spine.sql
@@ -1,6 +1,6 @@
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="cast('2023-01-01' as date)",
-    end_date="cast('2023-06-30' as date)"
+    start_date="cast('2024-01-01' as date)",
+    end_date="cast('2024-12-31' as date)"
    )
 }}
