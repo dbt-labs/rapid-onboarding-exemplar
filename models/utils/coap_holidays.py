14790490abcc6445dc7702eb34f261c26f03ceed
Kyle Coapman <kcoapman@fishtownanalytics.com>
2024-01-24 18:01:55 +0000

    update holidays python script

---
+++ models/utils/coap_holidays.py
@@ -0,0 +1,24 @@
+import holidays
+import pandas
+
+#all python models need to be defined at the start with this specific syntax
+def model(dbt, session):
+
+#python models don't use Jinja. Here we are using dbt.config to create model configurations
+#be sure to materialize python models as tables, and to specify the packages that were imported above
+    dbt.config(
+        materialized="table",
+        packages=['pandas', 'holidays']
+    )
+
+    us_holidays = holidays.US()
+
+#python models don't use Jinja. Here we are using dbt.ref to create model references
+    df = dbt.ref('date_spine').to_pandas()
+
+# if you are using snowpark columns need to be uppercase
+    df['IS_HOLIDAY'] = df['DATE_DAY'].apply(lambda date: date in us_holidays)
+
+    print(food)
+#in dbt, you always need to return your data frame at the end of your models
+    return df
