3bc015fa3872fea1e7dae485e1b7c2bd749817f9
Bethany Hipple <bethany.hipple@fishtownanalytics.com>
2024-06-17 18:14:36 +0000

    stg public

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -10,9 +10,10 @@ select
     -- descriptions
     paymentmethod as payment_method,
     status,
-    {{ money('amount') }} as amount -- amount is stored in cents, convert it to dollars
+    {{ money('amount') }} as amount, -- amount is stored in cents, convert it to dollars
     
     -- datetimes
+    created as created_at
 
 from {{ ref('snapshot_stg_payments') }} 
 -- pull only the most recent update for each unique record
