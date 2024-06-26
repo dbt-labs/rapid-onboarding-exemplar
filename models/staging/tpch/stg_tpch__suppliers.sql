49f35a19c45a67a6475e70e48e8a3d21ab63041f
Dan Horner <dan.horner@fishtownanalytics.com>
2024-06-26 20:56:24 +0000

    added static field to demonstrate slim ci

--- models/staging/tpch/stg_tpch__suppliers.sql
+++ models/staging/tpch/stg_tpch__suppliers.sql
@@ -7,7 +7,8 @@ with source as (
 renamed as (
 
     select
-
+        -- demonstrate Slim CI
+        1 as static_field,
         -- ids
         s_suppkey as supplier_id,
         s_nationkey as nation_id,
