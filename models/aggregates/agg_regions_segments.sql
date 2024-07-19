5f0794c2c7a425d9120af86b48103f52416330d3
Faith Lierheimer <faith.lierheimer@fishtownanalytics.com>
2024-07-19 15:30:16 +0000

    adding sales + 1

--- models/aggregates/agg_regions_segments.sql
+++ models/aggregates/agg_regions_segments.sql
@@ -11,7 +11,8 @@ orders as (
 agg_orders as (
     select
         customer_id,
-        sum(net_item_sales_amount) as total_sales
+        sum(net_item_sales_amount) as total_sales,
+        sum(net_item_sales_amount) + 1 as sales_plus_one
     from orders
     group by 1
 ),
@@ -20,10 +21,11 @@ final as (
     select
         customers.region,
         customers.market_segment,
-        round(sum(agg_orders.total_sales),2) as total_sales
+        round(sum(agg_orders.total_sales),2) as total_sales,
+        sales_plus_one
     from customers
     left join agg_orders on customers.customer_id = agg_orders.customer_id
-    group by 1, 2
+    group by 1, 2, 4
 )
 
 select * from final
