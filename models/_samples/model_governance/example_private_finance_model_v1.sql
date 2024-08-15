a9d0bc387b4b01270564fe6057e6e9bcd31b6bd8
Faith Lierheimer <faith.lierheimer@fishtownanalytics.com>
2024-08-15 20:44:28 +0000

    checking ci

--- models/_samples/model_governance/example_private_finance_model_v1.sql
+++ models/_samples/model_governance/example_private_finance_model_v1.sql
@@ -1,2 +1,14 @@
-select *
+select 
+    order_id,
+    order_date as order_time,
+    customer_id,
+    order_status_code,
+    priority_code,
+    clerk_name,
+    ship_priority,
+    order_count,
+    gross_item_sales_amount,
+    item_discount_amount,
+    item_tax_amount,
+    net_item_sales_amount
 from {{ ref('fct_orders') }}
--- models/_samples/model_governance/example_private_finance_model_v2.sql
+++ models/_samples/model_governance/example_private_finance_model_v2.sql
@@ -1,3 +1,13 @@
 select 
-    * exclude priority_code
+    order_id,
+    order_date as order_time,
+    customer_id,
+    order_status_code,
+    clerk_name,
+    ship_priority,
+    order_count,
+    gross_item_sales_amount,
+    item_discount_amount,
+    item_tax_amount,
+    net_item_sales_amount
 from {{ ref('fct_orders') }}
