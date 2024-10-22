b29c9ae386658cd3a724d289b42e3c45e515ea06
Oliver Ramsay <oliver.ramsay@icloud.com>
2024-10-22 10:24:54 +0000

    made some changes

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,18 +1,19 @@
 -- example showing staging model after snapshot
-
 select
     -- ids
     id as payment_id,
     orderid as order_id,
-    
+
     -- descriptions
     paymentmethod as payment_method,
     status,
-    {{ money('amount') }} as amount, -- amount is stored in cents, convert it to dollars
-    
+    {{ money("amount") }} as amount,  -- amount is stored in cents, convert it to dollars
+
     -- datetimes
     created as created_at
 
-from {{ ref('snapshot_stg_payments') }} 
+from {{ ref("snapshot_stg_payments") }}
 -- pull only the most recent update for each unique record
 where dbt_valid_to is null
+
+
