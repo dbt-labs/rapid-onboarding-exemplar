d84881a8b92c1321273aff8305071563027a3523
Keith Fajardo <keith.nicsonfajardo@dbtlabs.com>
2024-12-04 20:14:53 +0000

    commit changes

--- models/marts/dims/my_orders/dim_countries.sql
+++ models/marts/dims/my_orders/dim_countries.sql
@@ -8,11 +8,13 @@ with
 
 countries as (
 
-    select 1 as country_sk, 'United States'::varchar(100) as country_name union all
-    select 2 as country_sk, 'France'::varchar(100) as country_name union all
-    select 3 as country_sk, 'Italy'::varchar(100) as country_name union all
-    select 4 as country_sk, 'Philippines'::varchar(100) as country_name
-    -- union all
+    select 'United States'::varchar(100) as country_name
+
+    -- select 1 as country_sk, 'United States'::varchar(100) as country_name union all
+    -- select 2 as country_sk, 'France'::varchar(100) as country_name union all
+    -- select 3 as country_sk, 'Italy'::varchar(100) as country_name union all
+    -- select 4 as country_sk, 'Philippines'::varchar(100) as country_name
+    -- -- union all
     -- select 5 as country_sk, 'Italia'::varchar(100) as country_name
 
 )
