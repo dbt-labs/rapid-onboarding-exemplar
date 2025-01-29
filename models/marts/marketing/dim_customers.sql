adf40197b8df7d35f81ec1a2c9d3f35bbff79c08
Oliver Ramsay <oliver.ramsay@icloud.com>
2025-01-29 10:05:23 +0000

    changes

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,32 +1,25 @@
 {{
     config(
-        transient = false
+        schema = 'marts'
     )
 }}
 
 with customer as (
-
     select * from {{ ref('stg_tpch__customers') }}
-
 ),
 nation as (
-
     select * from {{ ref('stg_tpch__nations') }}
-
 ),
 region as (
-
     select * from {{ ref('stg_tpch__regions') }}
-
 ),
+
 final as (
     select
         customer.customer_id,
         customer.name,
         customer.address,
-        {# nation.nation_id as nation_id, #}
         nation.name as nation,
-        {# region.region_id as region_id, #}
         region.name as region,
         customer.phone_number,
         customer.account_balance,
@@ -38,9 +31,6 @@ final as (
         inner join region
             on nation.region_id = region.region_id
 )
-select
-    *
-from
-    final
-order by
-    customer_id
+
+select * from final
+order by customer_id
