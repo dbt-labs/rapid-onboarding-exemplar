88b9999a3eb214cd0c44a7a5fc84a81153f587b0
Jrobinson-dev <jamieir23+githubdbtlabs@gmail.com>
2026-02-12 07:54:22 +0800

    Auto commit for Studio handoff

--- models/canvas/custmers_count.sql
+++ models/canvas/custmers_count.sql
@@ -3,10 +3,25 @@ WITH dim_customers AS (
   SELECT
     *
   FROM {{ ref('rapid_onboarding_exemplar', 'dim_customers') }}
+), fct_orders AS (
+  /* orders fact table */
+  SELECT
+    *
+  FROM {{ ref('rapid_onboarding_exemplar', 'fct_orders') }}
+), "join" AS (
+  SELECT
+    *
+  FROM fct_orders
+  LEFT JOIN dim_customers
+    USING (CUSTOMER_ID)
 ), count_the_customers AS (
   SELECT
-    COUNT(DISTINCT CUSTOMER_ID) AS countd_CUSTOMER_ID
-  FROM dim_customers
+    CUSTOMER_ID,
+    SUM(ORDER_COUNT) AS sum_ORDER_COUNT,
+    SUM(NET_ITEM_SALES_AMOUNT) AS sum_NET_ITEM_SALES_AMOUNT
+  FROM "join"
+  GROUP BY
+    CUSTOMER_ID
 ), custmers_count_sql AS (
   SELECT
     *
